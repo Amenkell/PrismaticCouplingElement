7e71b77071dddf9349b606de2073ff6bfab6c4ee
Amenkell <win.kudymov@gmail.com>
2025-11-04 02:41:12 +0300

    Revert "fix(CD): пробуем бороться с i18"
    This reverts commit fa5a4960e8ece0720d72c61a94803f3155e948a8.

--- electron/main.js
+++ electron/main.js
@@ -3,9 +3,6 @@ const path = require('path');
 const url = require('url');
 const isDev = process.env.NODE_ENV === 'development';
 
-// Исправление ICU проблемы для упакованных приложений
-app.commandLine.appendSwitch('--disable-icu-data-url');
-
 require('@electron/remote/main').initialize();
 
 // Отключаем предупреждения безопасности в production
